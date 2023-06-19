a6dddb0142152e03c19c3cf8cda1701f31869169
JonasNevinskas <jonas.nevinskas@unrvld.com>
2023-06-19 10:06:11 +0300

    Add project files.

---
+++ StatybuWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StatybuWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
